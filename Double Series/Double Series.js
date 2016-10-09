4894f53f3b126d7bea5d45924e1b8bf5cbdb2f11
rorico <daniel77chen@hotmail.com>
2016-10-09 13:57:05 -0400

    sepatare settings into new file

--- Double Series/Double Series.js
+++ Double Series/Double Series.js
@@ -1,28 +1,3 @@
-//settings
-var maxGame = 1000;
-var speed = 00;
-var human = [false,false,false,false];
-var showMoves = false;
-//game play
-function playBlue(player) {
-    //return playRandom(player,1);
-    //return playHybrid(player,1);
-    //return playAI(player,1,true);
-    return playBest(player,1,true);
-    //return playAIphil(player,1,true);
-    //return playSides(player,1,true);
-    //return playWorth(player,1,true);      //currently doesn't work
-}
-function playGreen(player) {
-    //return playRandom(player,3);
-    //return playHybrid(player,3);
-    //return playAI(player,3,true);
-    return playBest(player,3,true);
-    //return playAIphil(player,3,true);
-    //return playSides(player,3,true);
-    //return playWorth(player,3,true);      //currently doesn't work
-}
-
 //start of ingame controls
 $('#speed').val(speed);
 $('#speed').keyup(function () {
