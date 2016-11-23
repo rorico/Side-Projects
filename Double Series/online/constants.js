3213af4b0100e3bc55f40b9744e9e3d17bd58ed9
rorico <daniel77chen@hotmail.com>
2016-11-23 17:55:21 -0500

    ^

---
+++ Double Series/online/constants.js
@@ -0,0 +1,10 @@
+var constants = {
+	PLAY_REPLACE:0,
+	PLAY_REMOVE:-1,
+	PLAY_ADD:1,
+	PLAY_FINISH:2
+}
+
+if (typeof module === "object" && module && typeof module.exports === "object") {
+	module.exports = constants;
+}
