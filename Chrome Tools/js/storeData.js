fceda62e46ba9aa0efb5393dfe39704b077c38b6
rorico <daniel77chen@hotmail.com>
2016-12-28 14:41:02 -0500

    move old storage data to local rather than sync

--- Chrome Tools/js/storeData.js
+++ Chrome Tools/js/storeData.js
@@ -3,6 +3,7 @@ var getData;
 (function() {
     var suf = "s";
     var indexSuf = "Indexes";
+    var metaSuf = "Meta";
     storeData = function(name,data) {
         var storeName = name + suf;
         chrome.storage.sync.get(storeName, function(items) {
@@ -29,6 +30,9 @@ var getData;
                 setObj[storeName] = info;
                 chrome.storage.sync.set(setObj, function() {
                     if (chrome.runtime.lastError) {
+                        if (chrome.runtime.lastError.message === "QUOTA_BYTES quota exceeded") {
+                            moveData(name,info,data);
+                        }
                         log(chrome.runtime.lastError);
                         return;
                     }
@@ -39,12 +43,17 @@ var getData;
 
     getData = function(name,callback) {
         var indexName = name + indexSuf;
-        chrome.storage.sync.get(indexName, function(item) {
+        var metaName = name + metaSuf;
+        chrome.storage.sync.get([indexName,metaName], function(item) {
             if (chrome.runtime.lastError) {
                 log(chrome.runtime.lastError);
                 return;
             }
             var indexes = item[indexName] || [];
+            var metaData = items[metaName] || {};
+            if (metaData.sync) {
+                indexes = indexes.slice(metaData.sync[0],metaData.sync[1]);
+            }
             indexes.push(name + suf);
             chrome.storage.sync.get(indexes, callback);
         });
@@ -52,7 +61,8 @@ var getData;
 
     function moveData(name,info,data) {
         var indexName = name + indexSuf;
-        chrome.storage.sync.get(indexName, function(items) {
+        var metaName = name + metaSuf;
+        chrome.storage.sync.get([indexName,metaName], function(items) {
             if (chrome.runtime.lastError) {
                 log(chrome.runtime.lastError);
                 return;
@@ -60,8 +70,10 @@ var getData;
             var indexes = items[indexName] || [];
             var dataName = name + "_" + indexes.length;
             indexes.push(dataName);
+
             var setObj = {indexes:indexes};
             setObj[indexName] = indexes;
+            setObj[metaName] = metaData;
             setObj[name + suf] = [];
             setObj[dataName] = info;
             chrome.storage.sync.set(setObj, function() {
@@ -71,6 +83,44 @@ var getData;
                 }
             });
             storeData(name,data);
+
+
+            //move older things to localStorage - unlimited storage
+            var metaData = items[metaName] || {};
+
+            //hold [start,end] values, end is 1 more than largest
+            if (!metaData.sync) {
+                metaData.sync = [0,indexes.length];
+            }
+            metaData.sync[1]++;
+            //limit to 5 blocks of data
+            var overflow = metaData.sync[1] - metaData.sync[0] - 5;
+            if (size > 0) {
+                if (!metaData.local) {
+                    metaData.local = [0,0];
+                }
+                metaData.local[1] += size;
+                var remove = indexes.slice(metaData[0],metaData[0] + size);
+                metaData.sync[0] += size;
+                chrome.storage.sync.get(remove, function(removedItems) {
+                    if (chrome.runtime.lastError) {
+                        log(chrome.runtime.lastError);
+                        return;
+                    }
+                    chrome.storage.local.set(removedItems,function() {
+                        if (chrome.runtime.lastError) {
+                            log(chrome.runtime.lastError);
+                            return;
+                        }
+                    });
+                    chrome.storage.sync.remove(remove,function()) {
+                        if (chrome.runtime.lastError) {
+                            log(chrome.runtime.lastError);
+                            return;
+                        }
+                    }
+                });
+            }
         });
     }
 })();
