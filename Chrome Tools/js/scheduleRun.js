ad9ec1069c6a7e8b7a34c720a9b487d44ae2b27d
rorico <daniel77chen@hotmail.com>
2017-01-22 20:08:07 -0500

    fix spacing

--- Chrome Tools/js/scheduleRun.js
+++ Chrome Tools/js/scheduleRun.js
@@ -1,8 +1,8 @@
 var weekSchedule;
 chrome.runtime.getBackgroundPage(function (backgroundPage) {
-	//structure this way so it is similar to content script
+    //structure this way so it is similar to content script
     weekSchedule = function(dates,callback) {
-    	callback(backgroundPage.weekSchedule(dates));
+        callback(backgroundPage.weekSchedule(dates));
     };
     scheduleInit($("body"));
 });
--- Chrome Tools/js/youtube.js
+++ Chrome Tools/js/youtube.js
@@ -1,43 +1,43 @@
 var youtube = (function() {
-	var pausedVideos = [];
-	return youtube;
+    var pausedVideos = [];
+    return youtube;
 
-	function youtube() {
-		//get all youtube tabs that isn't the current one
-		chrome.tabs.query({url:["*://*.youtube.com/*", "*://youtube.com/*"],active:false}, function(tabs) {
-			//if tabs is empty, nothing to play anyways
-			var cnt = 0;
-			var num = tabs.length;
-			var paused = false;
-			for (var i = 0 ; i < num ; i++) {
-				var tab = tabs[i];
-	            var data = {action:"pause"};
-	            var callback = function(id) {
-	            	return function(stopped) {
-						if (stopped) {
-							if (paused) {
-								pausedVideos.push(id);
-							} else {
-								pausedVideos = [id];
-							}
-							paused = true;
-						}
-						cnt++;
-						if (cnt === num && !paused) {
-							play();
-						}
-					};
-	            }(tab.id);
+    function youtube() {
+        //get all youtube tabs that isn't the current one
+        chrome.tabs.query({url:["*://*.youtube.com/*", "*://youtube.com/*"],active:false}, function(tabs) {
+            //if tabs is empty, nothing to play anyways
+            var cnt = 0;
+            var num = tabs.length;
+            var paused = false;
+            for (var i = 0 ; i < num ; i++) {
+                var tab = tabs[i];
+                var data = {action:"pause"};
+                var callback = function(id) {
+                    return function(stopped) {
+                        if (stopped) {
+                            if (paused) {
+                                pausedVideos.push(id);
+                            } else {
+                                pausedVideos = [id];
+                            }
+                            paused = true;
+                        }
+                        cnt++;
+                        if (cnt === num && !paused) {
+                            play();
+                        }
+                    };
+                }(tab.id);
 
-				chrome.tabs.sendMessage(tab.id,data,callback);
-			}
-		});
-	}
+                chrome.tabs.sendMessage(tab.id,data,callback);
+            }
+        });
+    }
 
-		for (var i = 0 ; i < pausedVideos.length ; i++) {
-	        var data = {action:"play"};
-			chrome.tabs.sendMessage(pausedVideos[i],data);
-		}
-		pausedVideos = [];
-	}
+        for (var i = 0 ; i < pausedVideos.length ; i++) {
+            var data = {action:"play"};
+            chrome.tabs.sendMessage(pausedVideos[i],data);
+        }
+        pausedVideos = [];
+    }
 })();
