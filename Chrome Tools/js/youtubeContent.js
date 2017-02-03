1bbaf89abef700f2382e3ac5e6f89623a1a7d8d9
rorico <daniel77chen@hotmail.com>
2017-02-02 20:42:57 -0500

    update youtube control to use proper commands
    not youtube api, controlling movie player

--- Chrome Tools/js/youtubeContent.js
+++ Chrome Tools/js/youtubeContent.js
@@ -1,30 +1,21 @@
 function getPlayer() {
-    //admittedly, this is quite sketchy, but the proper way is so much more complicated
-    var p = window.document.getElementsByClassName("ytp-play-button")[0];
-    if (p) {
-        var paused = p.attributes["aria-label"].value === "Play";
-        return {obj:p,paused:paused};
-    }
+    return window.document.getElementsByTagName("video")[0];
 }
 
 function pause() {
-    var ret = getPlayer();
-    if (ret) {
-        if (!ret.paused) {
-            ret.obj.click();
-            return true;
-        }
+    var p = getPlayer();
+    if (!p.paused) {
+        p.pause();
+        return true;
     }
     return false;
 }
 
 function play() {
-    var ret = getPlayer();
-    if (ret) {
-        if (ret.paused) {
-            ret.obj.click();
-            return true;
-        }
+    var p = getPlayer();
+    if (p.paused) {
+        p.play();
+        return true;
     }
     return false;
 }
