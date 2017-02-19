1db9b142ad903640c60d9078c1d475af9d6411ec
rorico <daniel77chen@hotmail.com>
2017-02-18 19:01:36 -0500

    content script blocks fullscreen

--- Chrome Tools/js/content.js
+++ Chrome Tools/js/content.js
@@ -1,5 +1,6 @@
 var currentBlock;   //holds current block type, as well as if block or going to be blocked
 var blockId = "chromeTools_block";
+var fullWait = false;
 
 function block(type,info,callback) {
     var blockScreen;
@@ -11,46 +12,55 @@ function block(type,info,callback) {
         blockScreen = $("<div id='" + blockId + "'></div>");
         $("body").append(blockScreen);
     }
-    return blockScreen ? blockType(blockScreen,type,info,callback) : false;
+    if (blockScreen) {
+        //assuming this is run on chrome
+        var full = document.webkitFullscreenElement;
+        if (full) {
+            //can't append to iframes, so just force close fullscreen
+            if (full.tagName === "IFRAME") {
+                document.webkitExitFullscreen();
+            } else {
+                blockScreen.detach().appendTo(full);
+                //don't make more than 1 listener
+                if (!fullWait) {
+                    fullWait = true;
+                    $(document).one("webkitfullscreenchange",function() {
+                        fullWait = false;
+                        if (currentBlock) {
+                            blockScreen.detach().appendTo("body").focus();
+                            if (currentBlock === "time") {
+                                timeLineResize();
+                            }
+                        }
+                    });
+                }
+            }
+        }
+        return blockType(blockScreen,type,info,callback);
+    }
+    return false;
 }
 
 //only helper function for block
 function blockType(blockScreen,type,info,callback) {
     currentBlock = type;
-    var funct = function() {
-        if (type === "time") {
-            if (typeof timeLineInit === "undefined" || typeof keyPressInit === "undefined") {
-                console.log(type + " content script missing");
-            } else {
-                keyPressInit(blockScreen);
-                timeLineInit(blockScreen,info);
-            }
+    if (type === "time") {
+        if (typeof timeLineInit === "undefined" || typeof keyPressInit === "undefined") {
+            console.log(type + " content script missing");
         } else {
-            if (typeof scheduleInit === "undefined") {
-                console.log(type + " content script missing");
-            } else {
-                scheduleInit(blockScreen);
-            }
+            keyPressInit(blockScreen);
+            timeLineInit(blockScreen,info);
         }
-    };
-
-    var isFullScreen = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-    if (isFullScreen) {
-        $(document).one("webkitfullscreenchange mozfullscreenchange fullscreenchange",function() {
-            if (currentBlock) {
-                callback(true);
-                funct();
-            } else {
-                //probably means that page was unblocked
-                callback(false);
-            }
-        });
-        return true;
     } else {
-        callback(true);
-        funct();
-        return false;
+        if (typeof scheduleInit === "undefined") {
+            console.log(type + " content script missing");
+        } else {
+            scheduleInit(blockScreen);
+        }
     }
+
+    callback(true);
+    return false;
 }
 
 function unblock() {
