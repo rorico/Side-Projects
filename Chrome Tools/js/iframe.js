4a8001e00fa1609e059f52e532b36dd1ba35f028
rorico <daniel77chen@hotmail.com>
2017-02-25 09:50:19 -0500

    refactor of iframe, make height and width based on space, and responsive

--- Chrome Tools/js/iframe.js
+++ Chrome Tools/js/iframe.js
@@ -1,21 +1,53 @@
-function iframe(container,urls) {
-    var html = "<div>";
-    for (var i = 0 ; i < urls.length ; i++) {
-        html += "<iframe class='iframe' src=" + urls[i] + " tabindex='-1' sandbox='allow-same-origin allow-popups allow-forms allow-scripts' scrolling='no' ></iframe>";
-    }
-    html += "</div>";
-    container.append(html);
-    var width = Math.min(400,(container.width() - 100)/urls.length);
-    //want to get height programitically, will do for now
-    $(".iframe").width(width).height(500);
+var iframe;
+(function() {
+    var iframeId = "chromeTools_iframe";
+    var width;
+    var height = 0;
+    iframe = init;
 
-    //some iframes take control once they load, stop that
-    //http://stackoverflow.com/a/28932220
-    $(document).one('focusout', function(){
-        setTimeout(function(){
-            if (document.activeElement instanceof HTMLIFrameElement) {
-                container.focus();
+    function init(container,urls) {
+        var html = "<div id='" + iframeId + "'>";
+        for (var i = 0 ; i < urls.length ; i++) {
+            html += "<iframe class='iframe' src=" + urls[i] + " tabindex='-1' sandbox='allow-same-origin allow-popups allow-forms allow-scripts' scrolling='no' ></iframe>";
+        }
+        html += "</div>";
+        container.append(html);
+        resize();
+        //add responsiveness
+        $(window).resize(resize);
+
+        function resize() {
+            var newWidth = Math.min(400,roundTo(container.width()/urls.length - 50,50));
+            if (newWidth !== width) {
+                width = newWidth;
+                $(".iframe").width(width);
             }
-        },0);
-    })
-}
+            var last = container.children().last();
+            var bottom = last.height() + last.offset().top;
+            var maxHeight = (container.height() - bottom) * 2 + $("#" + iframeId).height();
+            var newHeight = Math.min(500,roundTo(maxHeight - 120,50));
+            if (newHeight !== height) {
+                height = newHeight;
+                $(".iframe").height(height);
+            }
+        }
+
+        //some iframes take control once they load, stop that
+        //http://stackoverflow.com/a/28932220
+        $(document).one('focusout', function(){
+            setTimeout(function(){
+                if (document.activeElement instanceof HTMLIFrameElement) {
+                    container.focus();
+                }
+            },0);
+        });
+    }
+
+    function roundTo(num,round) {
+        var div = num % round;
+        if (div > round/2) {
+            num += round;
+        }
+        return num - div;
+    }
+})();
