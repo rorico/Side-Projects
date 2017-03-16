05761b9461befff786f262f3bd42df7e41639654
rorico <daniel77chen@hotmail.com>
2017-03-16 19:31:50 -0400

    limit header rewrites to specific urls

--- Chrome Tools/js/iframeUrls.js
+++ Chrome Tools/js/iframeUrls.js
@@ -7,25 +7,25 @@ function setIframeUrls() {
         }
         if (items.iframeUrls) {
             iframeUrls = items.iframeUrls;
+
+            //workaround for sameorigin problem
+            chrome.webRequest.onHeadersReceived.addListener(function(e) {
+                var headers = e.responseHeaders || [];
+                for (var i = headers.length - 1 ; i >= 0 ; i--) {
+                    var header = headers[i].name.toLowerCase();
+                    if (header === "x-frame-options" || header === "frame-options") {
+                        headers.splice(i,1);
+                    }
+                }
+                return {
+                    responseHeaders: headers
+                }
+            }, {
+                urls: iframeUrls,
+                types: ["sub_frame"]
+            }, ["blocking", "responseHeaders"]);
         } else {
             iframeUrls = [];
         }
     });
 }
-
-//workaround for sameorigin problem
-chrome.webRequest.onHeadersReceived.addListener(function(e) {
-    var headers = e.responseHeaders || [];
-    for (var i = headers.length - 1 ; i >= 0 ; i--) {
-        var header = headers[i].name.toLowerCase();
-        if (header === "x-frame-options" || header === "frame-options") {
-            headers.splice(i,1);
-        }
-    }
-    return {
-        responseHeaders: headers
-    }
-}, {
-    urls: ["*://*/*"],
-    types: ["sub_frame"]
-}, ["blocking", "responseHeaders"]);
