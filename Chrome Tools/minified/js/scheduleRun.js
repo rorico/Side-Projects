35204a7ca638f71c80592e9cbe7c0b237139a9fa
rorico <daniel77chen@hotmail.com>
2017-03-27 20:11:56 -0400

    build 4.3 chromeTools

--- Chrome Tools/minified/js/scheduleRun.js
+++ Chrome Tools/minified/js/scheduleRun.js
@@ -1,1 +1,1 @@
-var weekSchedule;chrome.runtime.getBackgroundPage(function(e){weekSchedule=function(c,u){u(e.weekSchedule(c))},scheduleInit($("body"))});
+chrome.runtime.getBackgroundPage(function(e){scheduleInit($("body"),e)});
